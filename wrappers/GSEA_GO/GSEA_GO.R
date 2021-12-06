5d7f6a72e803b6d6ff727098766d4118707bd5c7
Nicolas <nicolas.blavet@ceitec.muni.cz>
2021-12-06 15:20:42 +0100

    update

--- wrappers/GSEA_GO/GSEA_GO.R
+++ wrappers/GSEA_GO/GSEA_GO.R
@@ -7,11 +7,15 @@ run_all <- function(args){
   cutoff_log2fc <- as.numeric(args[5])
   cutoff_padj <- as.numeric(args[6])
   n_up <- as.integer(args[7])
-  COLORS <- unlist(strsplit(args[8],split=":"))[1]
-  enrich_padj <- as.numeric(args[9])
-  enrich_padjmethod <- args[10]
-  enrich_minGSSize <- as.numeric(args[11])
-  enrich_maxGSSize <- as.numeric(args[12])
+  n_down <- as.integer(args[8])
+  COLORS <- unlist(strsplit(args[9],split=":"))
+  gsea_padj <- as.numeric(args[10])
+  gsea_padjmethod <- args[11]
+  gsea_minGSSize <- as.numeric(args[12])
+  gsea_maxGSSize <- as.numeric(args[13])
+  gsea_eps <- as.numeric(args[14])
+  gsea_nPermSimple <- as.numeric(args[15])
+  gsea_by <- args[16]
 
   library("data.table")
   library("clusterProfiler")
@@ -32,12 +36,28 @@ run_all <- function(args){
   de <- fread(input_genes,header = T)
   colnames(de)[colnames(de) == 'V1'] <- 'Geneid' # In case we need to rename
   ## filter genes
-  deseq_cutoff <- function(deseq = deseq2_tab, LOG2FC = 1, PADJ = 0.05){
+  deseq_cutoff <- function(deseq = deseq2_tab, LOG2FC = 0, PADJ = 1){
     x <- deseq[is.na(padj) == F & is.na(pvalue) == F,]
     x <- x[abs(log2FoldChange) >= LOG2FC & padj <= PADJ,]
     return(x)
   }
   deseq2_tab <- deseq_cutoff(de, cutoff_log2fc, cutoff_padj)
+
+  ## select just entrez id and stat/logFC
+  genes <- deseq2_tab[,.(Geneid, logFC = log2FoldChange)]
+  ## remove NA values
+  genes <- na.omit(genes)
+  ## order by decreasing logFC
+  setorder(genes, -logFC)
+
+  ## change into named vector
+  rankGenes <- genes$logFC
+  names(rankGenes) <- genes$Geneid
+
+    if(dir.exists(OUTPUT_DIR)==F){
+    dir.create(OUTPUT_DIR, recursive = T)
+  }
+
 
   if(dir.exists(OUTPUT_DIR)==F){
     dir.create(OUTPUT_DIR, recursive = T)
@@ -49,129 +69,151 @@ run_all <- function(args){
   deseq2_tab <- merge(deseq2_tab, universe, by.x = "Geneid", by.y = KEYID, all.x=T)
   fwrite(deseq2_tab[,.(Geneid, gene_name, ENTREZID, log2FoldChange, padj)], file = paste0(OUTPUT_DIR,"/Gene_ID.tsv"), sep="\t")
 
-  egoBP <- enrichGO(gene          = deseq2_tab$Geneid,
-                    universe      = universe[,KEYID],
+  gseGOBP <- gseGO(gene          = rankGenes,
                     OrgDb         = database,
                     keyType       = KEYID,
                     ont           = "BP", # "MF", "BP", and "CC", "ALL" (?)
-                    pAdjustMethod = enrich_padjmethod,
-                    pvalueCutoff  = enrich_padj,
+                    pAdjustMethod = gsea_padjmethod,
+                    pvalueCutoff  = gsea_padj,
                     readable      = FALSE,
-                    minGSSize     = enrich_minGSSize,
-                    maxGSSize     = enrich_maxGSSize)
+                    minGSSize     = gsea_minGSSize,
+                    maxGSSize     = gsea_maxGSSize,
+                    nPermSimple   = gsea_nPermSimple,
+                    eps           = gsea_eps,
+                    by            = gsea_by)
 
-  dtegoBP <- as.data.table(egoBP)
-  fwrite(dtegoBP, file = paste0(OUTPUT_DIR,"/GO_enrich_BP.tsv"), sep="\t")
+  dtgseGOBP <- as.data.table(gseGOBP)
+  fwrite(dtgseGOBP, file = paste0(OUTPUT_DIR,"/GSEA_GO_BP.tsv"), sep="\t")
 
-  egoMF <- enrichGO(gene          = deseq2_tab$Geneid,
-                    universe      = universe[,KEYID],
+  gseGOMF <- gseGO(gene          = rankGenes,
                     OrgDb         = database,
                     keyType       = KEYID,
                     ont           = "MF", # "MF", "BP", and "CC", "ALL" (?)
-                    pAdjustMethod = enrich_padjmethod,
-                    pvalueCutoff  = enrich_padj,
+                    pAdjustMethod = gsea_padjmethod,
+                    pvalueCutoff  = gsea_padj,
                     readable      = FALSE,
-                    minGSSize     = enrich_minGSSize,
-                    maxGSSize     = enrich_maxGSSize)
+                    minGSSize     = gsea_minGSSize,
+                    maxGSSize     = gsea_maxGSSize,
+                    nPermSimple   = gsea_nPermSimple,
+                    eps           = gsea_eps,
+                    by            = gsea_by)
 
-  dtegoMF <- as.data.table(egoMF)
-  fwrite(dtegoMF, file = paste0(OUTPUT_DIR,"/GO_enrich_MF.tsv"), sep="\t")
+  dtgseGOMF <- as.data.table(gseGOMF)
+  fwrite(dtgseGOMF, file = paste0(OUTPUT_DIR,"/GSEA_GO_MF.tsv"), sep="\t")
 
-  egoCC <- enrichGO(gene          = deseq2_tab$Geneid,
-                    universe      = universe[,KEYID],
+  gseGOCC <- gseGO(gene          = rankGenes,
                     OrgDb         = database,
                     keyType       = KEYID,
                     ont           = "CC", # "MF", "BP", and "CC", "ALL" (?)
-                    pAdjustMethod = enrich_padjmethod,
-                    pvalueCutoff  = enrich_padj,
+                    pAdjustMethod = gsea_padjmethod,
+                    pvalueCutoff  = gsea_padj,
                     readable      = FALSE,
-                    minGSSize     = enrich_minGSSize,
-                    maxGSSize     = enrich_maxGSSize)
+                    minGSSize     = gsea_minGSSize,
+                    maxGSSize     = gsea_maxGSSize,
+                    nPermSimple   = gsea_nPermSimple,
+                    eps           = gsea_eps,
+                    by            = gsea_by)
 
-  dtegoCC <- as.data.table(egoCC)
-  fwrite(dtegoCC, file = paste0(OUTPUT_DIR,"/GO_enrich_CC.tsv"), sep="\t")
+  dtgseGOCC <- as.data.table(gseGOCC)
+  fwrite(dtgseGOCC, file = paste0(OUTPUT_DIR,"/GSEA_GO_CC.tsv"), sep="\t")
 
   # Plot enrichment plot
-  myEnrichPlot <- function(go.table = dtegoBP,
+  myGSEAPlot <- function(fgsea.table = dtgseGOBP,
                          nUp = 10,
+                         nDown = 10,
                          GOPADJ = 0.05,
-                         mycol = "firebrick",
+                         gradient = c("firebrick","white","royalblue"),
                          ploTitle = "GO - Biological Processes"){
-    go.table <- go.table[p.adjust <= GOPADJ,]
-    setorder(go.table, p.adjust)
+    fgsea.table <- fgsea.table[p.adjust <= Padj,]
+    setorder(fgsea.table, p.adjust)
+    fgsea.table[, Enrichment := ifelse(NES > 0, "Up-regulated", "Down-regulated")]
 
-    nUp <- ifelse(nUp < 0, 10, nUp)
-    nUp <- ifelse(nUp > length(go.table[, Count]),
-                length(go.table[, Count]),nUp)
+    nUp <- ifelse(nUp < 0, 0, nUp)
+    nUp <- ifelse(nUp > length(fgsea.table[NES > 0, NES]),
+                  length(fgsea.table[NES > 0, NES]),nUp)
 
-    filtRes <- head(go.table, n = nUp)
+    nDown <- ifelse(nDown < 0, 0, nDown)
+    nDown <- ifelse(nDown > length(fgsea.table[NES < 0, NES]),
+                    length(fgsea.table[NES < 0, NES]),nDown)
 
-    g <- ggplot(filtRes, aes(reorder(Description, -p.adjust), Count)) +
-      geom_col(fill = mycol) +
+    filtRes <- rbind(head(fgsea.table, n = nUp),
+                     tail(fgsea.table, n = nDown ))
+
+    g <- ggplot(filtRes, aes(reorder(Description, NES), NES)) +
+      geom_col( aes(fill = NES )) +
       coord_flip() +
-      labs(x="", y="Count",
+      labs(x="", y="Normalized Enrichment Score",
            title=ploTitle) +
       theme_minimal() +
+      scale_fill_gradient2(high = gradient[1],
+                           mid = gradient[2],
+                           low = gradient[3],
+                           midpoint = 0) +
       theme(legend.position = "none")
 
     return(g)
     }
 
-  GO_BP_plot <- myEnrichPlot(dtegoBP,
-                             nUp = n_up,
-                             GOPADJ = enrich_padj,
-                             mycol = COLORS,
-                             ploTitle = "GO - Biological Process")
-  ggsave(GO_BP_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_BP.pdf",sep=""),
-       width = 10, height = 7, device = "pdf")
-  ggsave(GO_BP_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_BP.svg",sep=""),
-       width = 10, height = 7, device = svg, bg='transparent')
-  ggsave(GO_BP_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_BP.png",sep=""),
-       width = 10, height = 7, device = "png", bg='transparent')
+  GSEA_BP_plot <- myEnrichPlot(dtgseGOBP,
+                               nUp = n_up,
+                               nDown = n_dn,
+                               Padj = gsea_padj,
+                               gradient = COLORS,
+                               ploTitle = "GSEA - Biological Process")
+  ggsave(GSEA_BP_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_BP.pdf",sep=""),
+         width = 10, height = 7, device = "pdf")
+  ggsave(GSEA_BP_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_BP.svg",sep=""),
+         width = 10, height = 7, device = svg, bg='transparent')
+  ggsave(GSEA_BP_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_BP.png",sep=""),
+         width = 10, height = 7, device = "png", bg='transparent')
 
-  GO_MF_plot <- myEnrichPlot(dtegoMF,
-                           nUp = n_up,
-                           GOPADJ = enrich_padj,
-                           mycol = COLORS,
-                           ploTitle = "GO - Molecular Function")
-  ggsave(GO_MF_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_MF.pdf",sep=""),
-       width = 10, height = 7, device = "pdf")
-  ggsave(GO_MF_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_MF.svg",sep=""),
-       width = 10, height = 7, device = svg, bg='transparent')
-  ggsave(GO_MF_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_MF.png",sep=""),
-       width = 10, height = 7, device = "png", bg='transparent')
+  GSEA_MF_plot <- myEnrichPlot(dtgseGOMF,
+                               nUp = n_up,
+                               nDown = n_dn,
+                               Padj = gsea_padj,
+                               gradient = COLORS,
+                               ploTitle = "GSEA - Molecular Function")
+  ggsave(GSEA_MF_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_MF.pdf",sep=""),
+         width = 10, height = 7, device = "pdf")
+  ggsave(GSEA_MF_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_MF.svg",sep=""),
+         width = 10, height = 7, device = svg, bg='transparent')
+  ggsave(GSEA_MF_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_MF.png",sep=""),
+         width = 10, height = 7, device = "png", bg='transparent')
 
-  GO_CC_plot <- myEnrichPlot(dtegoCC,
-                           nUp = n_up,
-                           GOPADJ = enrich_padj,
-                           mycol = COLORS,
-                           ploTitle = "GO - Cellular Componenet")
-  ggsave(GO_CC_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_CC.pdf",sep=""),
-       width = 10, height = 7, device = "pdf")
-  ggsave(GO_CC_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_CC.svg",sep=""),
-       width = 10, height = 7, device = svg, bg='transparent')
-  ggsave(GO_CC_plot, filename = paste0(OUTPUT_DIR,"/GO_enrich_CC.png",sep=""),
-       width = 10, height = 7, device = "png", bg='transparent')
-
-  fwrite(deseq2_tab[,.(Geneid, gene_name, ENTREZID, log2FoldChange, padj)], file = paste0(OUTPUT_DIR,"/GO_enrichment_GeneID.tsv"), sep="\t")
+  GSEA_CC_plot <- myEnrichPlot(dtgseGOCC,
+                               nUp = n_up,
+                               nDown = n_dn,
+                               Padj = gsea_padj,
+                               gradient = COLORS,
+                               ploTitle = "GSEA - Cellular Component")
+  ggsave(GSEA_CC_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_CC.pdf",sep=""),
+         width = 10, height = 7, device = "pdf")
+  ggsave(GSEA_CC_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_CC.svg",sep=""),
+         width = 10, height = 7, device = svg, bg='transparent')
+  ggsave(GSEA_CC_plot, filename = paste0(OUTPUT_DIR,"/GSEA_GO_CC.png",sep=""),
+         width = 10, height = 7, device = "png", bg='transparent')
 
 }
 
 # run as Rscript
 args <- commandArgs(trailingOnly = T)
 
-# args <- character(12)
+# args <- character(16)
 # args[1] <- "E:/OneDrive - MUNI/TF_Daniel" # WORKDIR
 # args[2] <- "DESeq2.tsv" # input_genes
-# args[3] <- "clusterProfiler" # OUTPUT_DIR
+# args[3] <- "GSEA_GO" # OUTPUT_DIR
 # args[4] <- "org.Hs.eg.db" # organism
-# args[5] <- "1" # cutoff_log2fc
-# args[6] <- "0.05" # cutoff_padj
+# args[5] <- "0" # cutoff_log2fc
+# args[6] <- "1" # cutoff_padj
 # args[7] <- 10 # n_up
-# args[8] <- "firebrick:white:royalblue" # colors
-# args[9] <- "0.1" # enrich_padj = padj from the enrich result table
-# args[10] <- "BH" # enrich_padjmethod (BH,BY,fdr,holm,hochberg,hommel,bonferroni,none)
-# args[11] <- "2" # enrich_minGSSize
-# args[12] <- "Inf" # enrich_maxGSSize
+# args[8] <- 10 # n_down
+# args[9] <- "firebrick:white:royalblue" # colors
+# args[10] <- "0.1" # gsea_padj = padj from the enrich result table
+# args[11] <- "BH" # gsea_padjmethod (BH,BY,fdr,holm,hochberg,hommel,bonferroni,none)
+# args[12] <- "2" # gsea_minGSSize
+# args[13] <- "Inf" # gsea_maxGSSize
+# args[14] <- "0" # gsea_eps
+# args[15] <- 10000 # gsea_nPermSimple
+# args[16] <- "fgsea" # gsea_by ("fgsea","DOSE")
 
 run_all(args)
